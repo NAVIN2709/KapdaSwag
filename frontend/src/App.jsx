adb29968f33e266dfdcf693e3845d898de925cb0
Naveen <naveen@Naveens-MacBook-Pro.local>
2025-03-20 22:27:09 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/signup";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
